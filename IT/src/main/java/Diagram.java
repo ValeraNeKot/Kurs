5d9c16b017a2d657d6ede299641998c3a3cc4011
Valera <kukuklerahello@mail.ru>
2024-11-16 19:17:50 +0300

    commit2

--- IT/src/main/java/Diagram.java
+++ IT/src/main/java/Diagram.java
@@ -21,6 +21,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class Diagram implements Initializable {
+	//,t,t
     public Button buttonBack;
     public BarChart Chart;
     public CategoryAxis categoryAxis;
